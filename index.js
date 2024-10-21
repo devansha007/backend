da7463afc6dd3aef6884db228a16d0558d262e8c
devansha007 <adevansh130@gmail.com>
2024-10-21 12:54:33 +0530

    homecoming

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const fs = require("fs")
 const http = require("http")
 const PORT = 3000;
-const HOST_NAME = "localhost";
+
 const home = fs.readFileSync('./index.html')
 
 
 const app = http.createServer((req,res)=>{
     res.end(home)
 })
-app.listen(PORT,HOST_NAME,()=>{
+app.listen(PORT,()=>{
     console.log(`the app is being listened on port ${PORT}`)
 })
