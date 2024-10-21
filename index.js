a7408f1f486948587d2754462cde19018bab7849
devansha007 <adevansh130@gmail.com>
2024-10-21 16:32:07 +0530

    uu

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const home = fs.readFileSync('./index.html')
 
 
 const app = http.createServer((req,res)=>{
-    res.end(home)
+    if(req.url === '/favicon.ico'){ return res.end()}
+    if(req.url === '/'){ return res.end(home)}
+    if(req.url === '/contact'){ return res.end('<h1>hii this is contact page</h1>')}
+    if(req.url === '/about'){ return res.end('<h1>hii this is about page</h1>')}
+    
+    console.log(req.url)
 })
 app.listen(PORT,()=>{
     console.log(`the app is being listened on port ${PORT}`)
